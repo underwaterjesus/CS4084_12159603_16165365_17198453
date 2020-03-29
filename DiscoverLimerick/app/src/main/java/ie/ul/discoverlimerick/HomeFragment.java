3f3b897bce6204523b15ebafd7d189b6d09f3335
underwaterjesus <12159603@studentmail.ul.ie>
2020-03-29 04:53:13 +0100

    Initial App Creation: -UI Elements:navigation drawer(different versions for logged in/logged out user), action bar -Fragments started: home menu, category menu -Recycler Views & adapters implemented for home & category menus -Custom layouts for menu items -Navigation from home -> category menu working -Asks for location permissions -Can use device coordinares -Category shows distance from current device to location item(if location enabled) TO DO: -Location details fragment -Read reviews fragment -Leave review fragment -Map activity -'Near Me' function

---
+++ DiscoverLimerick/app/src/main/java/ie/ul/discoverlimerick/HomeFragment.java
@@ -0,0 +1,48 @@
+package ie.ul.discoverlimerick;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Arrays;
+
+public class HomeFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private CycleAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.category_recycler);
+        layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new CycleAdapter(MainActivity.CATEGORIES);
+        recyclerView.setAdapter(mAdapter);
+        mAdapter.setOnItemClickListener(new CycleAdapter.OnItemClickListner() {
+            @Override
+            public void onItemClick(int position) {
+                MainActivity.selected_category = MainActivity.CATEGORIES[position].getCategoryName();
+                MainActivity.showToast(getContext(), MainActivity.CATEGORIES[position].getCategoryName());
+                FragmentTransaction tranny = getActivity().getSupportFragmentManager().beginTransaction();
+                tranny.replace(R.id.fragment_container, new CategoryFragment());
+                tranny.addToBackStack(null);
+                tranny.commit();
+            }
+        });
+
+        return view;
+    }
+}
---
+++ DiscoverLimerick/app/src/main/java/ie/ul/discoverlimerick/Review.java
@@ -0,0 +1,4 @@
+package ie.ul.discoverlimerick;
+
+public class Review {
+}
