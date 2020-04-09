cb78c831aec5a33c6b1a1f4debbc989a96f735b6
underwaterjesus <12159603@studentmail.ul.ie>
2020-04-10 00:49:36 +0100

    Homescreen: added ViewFlipper image carousel

--- app/src/main/java/ie/ul/discoverlimerick/MainActivity.java
+++ app/src/main/java/ie/ul/discoverlimerick/MainActivity.java
@@ -81,6 +81,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -171,7 +172,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            if(sendHome){
+            if (sendHome) {
                 sendHome = false;
                 goHome();
                 return;
